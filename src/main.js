870c72057de7d6fed344ee7a6f7e30844f65ea64
Dzoibo <ivandztitan@gmail.com>
2024-03-02 11:45:21 +0100

    feat: add ping animation

--- src/main.js
+++ src/main.js
@@ -1,2 +1,30 @@
 import './assets/style.scss';
-console.log('Hello from Vite!');
+import gsap from "gsap";
+
+/**
+ * coded by ivan DZOIBO
+ * github: https://github.com/dzoibo/Apple-HomePod-mini.git
+ * 
+ */
+
+const homepodMainContainer= document.querySelector(".pod-main-container");
+const discoverBtn= document.querySelector(".btn-discover");
+const whiteGradiant= document.querySelector(".main-gradiant");
+const initialTitle= document.querySelector(".initial-title");
+const mainTitle = document.querySelector(".main-title");
+const siriTitle= document.querySelector(".siri-title");
+
+discoverBtn.addEventListener("click",()=>{
+    discoverBtn.style.opacity=0;
+    whiteGradiant.style.opacity=0
+    gsap.to(homepodMainContainer,{
+        top: 350,
+        width: 489,
+        height: 449
+    });
+    gsap.to(initialTitle,{
+        scale: 0.5,
+        top: 350
+    })
+    gsap.to(siriTitle,{opacity:1})
+})
